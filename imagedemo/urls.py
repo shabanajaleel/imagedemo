d4907233885239fd6bbbfb5b51036ed718fc2d29
shabanajaleel <shabanac71@gmail.com>
2022-02-02 14:58:33 +0530

    next commit

--- imagedemo/urls.py
+++ imagedemo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[ 
     path('',views.home,name="home"),
-    path('file',views.file,name="file")
+    path('file/',views.file,name="file"),
+    path('addproducts/',views.addproducts,name="addproducts"),
 ]
--- imagedemo/views.py
+++ imagedemo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . models import *
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -13,3 +14,15 @@ def file(request):
     context={'products':products}
     
     return render(request,'fileimage.html',context)
+
+def addproducts(request):
+    if request.method=="POST":
+        prod_name=request.POST['name']
+        price=request.POST['price']
+        images=request.FILES.getlist('images')
+        for image in images:
+            product=Products(name=prod_name,price=price,image=image).save()
+        messages.success(request,"product inserted successfully")
+        
+
+    return render(request,'addproducts.html')
